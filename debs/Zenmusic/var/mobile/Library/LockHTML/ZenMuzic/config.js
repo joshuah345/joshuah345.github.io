e4d4b8182f61fa57430e6ca0b51e7aac43b5796d
Joshua Henry <joshuah345@gmail.com>
2019-09-03 03:14:12 -0400

    added player for hs

---
+++ debs/Zenmusic/var/mobile/Library/LockHTML/ZenMuzic/config.js
@@ -0,0 +1,4 @@
+var TrackColor = "#EEEEEE"; // color for Track name
+var ArtistColor = "#adadad"; // color for artist name
+var BGColor = "#1c1c1c"; // background color. (can also use rgba(x,y,z,a) for transparency)
+var Blur = "0"; // how much to blur the background (if transparent) in px
