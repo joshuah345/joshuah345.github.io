0351703d289cbdf80a37d8499acc2e97fa604829
Joshua Henry <joshuah345@gmail.com>
2019-09-03 22:05:20 -0400

    sbbox update

---
+++ debs/BetterSBBox/var/mobile/Library/SBHTML/BetterSBBox/Config.js
@@ -0,0 +1,8 @@
+var BoxWidth = "50"; //   Width of the box (percentage of screen width)
+var BoxHeight = "50"; // height of the box (percentage of screen height)
+var BorderRadius = "0"; // how rounded the corners should be (in px)
+var BorderWidth = "10"; // Width of the border (in px), set to 0 to hide
+var BorderColor = "rgba(0,0,0,1)"; // rgba valur for border color
+var BGColor = "rgba(50,50,50,0.6)"; // rgba value for the background color. Alpha must be in the range 0.0-1.0
+var BlurAmount = "1"; // how much to blur the background (in px)
+var BorderStyle = "solid"; // type of border the box has [dotted, solid, dashed, double, groove, ridge, inset, outset, none ]   You can set one for each side e.g  "top, right, bottom, left"
