d5b9c775deaa9f740d2a7ff6c6917dd1737413fc
hiroshi <joshuah345@gmail.com>
2024-05-14 15:44:18 -0400

    slight modification to redirect

--- gh/main.js
+++ gh/main.js
@@ -2,17 +2,19 @@ function currentPage(uri) {
     if (uri === undefined) {
         return window.location.href
     } else {
-        window.location.replace(uri)
+        window.location.href = `${uri}`
     }
 }
 
+
 function ghRedirect() {
     if (currentPage().toLowerCase().includes("/gh/")) {
-        let trimURL = window.location.pathname.replace("/gh/","joshuah345/")
-        let newURL = ("https://github.com/" + trimURL)
+        const trimURL = window.location.pathname.replace("/gh/","joshuah345/")
+        const newURL = ("https://github.com/" + trimURL)
         console.log("redirecting to github...")
         currentPage(newURL)
     } else {
         currentPage(window.location.origin)
     }
 }
+
